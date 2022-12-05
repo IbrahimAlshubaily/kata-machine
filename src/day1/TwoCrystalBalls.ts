0629f9aedd4c96c5c60709ec74094aa881d3958f
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-12-05 10:50:10 +0000

    O(sqrt(N)) search

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,10 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const stepSize = Math.floor(Math.sqrt(breaks.length));
+    let currIdx = stepSize;
 
+    while (!breaks[currIdx] && currIdx < breaks.length) currIdx += stepSize;
+    currIdx -= stepSize;
+    while (!breaks[currIdx] && currIdx < breaks.length) currIdx++;
+
+    return currIdx === breaks.length ? -1 : currIdx;
 }
