05842b214d9bb02e557ed08652c4ca897ade8f95
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-12-05 10:14:41 +0000

    BinarySearch

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,13 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    let out = false;
+    for (let i = 0; i < needle; i++) {
+        if (haystack[i] === needle) {
+            out = true;
+            break;
+        }
+    }
+    return out;
 }
