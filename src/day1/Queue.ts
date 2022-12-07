9452c7b1ebf299cfdcc368b82ad70d7d90c8464f
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-12-07 10:10:55 +0000

    CleanQ

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,37 @@
+import SinglyLinkedList from "./SinglyLinkedList";
+
 export default class Queue<T> {
     public length: number;
-
-    
-
+    private head?: SinglyListNode<T>;
+    private tail?: SinglyListNode<T>;
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node = { value: item } as SinglyListNode<T>;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = this.tail.next;
+    }
 
-}
     deque(): T | undefined {
+        if (!this.head) {
+            return;
+        }
+        this.length--;
+        const out = this.head.value;
+        this.head = this.head.next;
+        if (this.length === 0) this.tail = undefined;
+        return out;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
